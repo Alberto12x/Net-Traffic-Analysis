abed463198f6c19c2a65bba6374911a2ea07da1e
Alberto12x <alberm33@ucm.es>
2024-12-14 22:14:27 +0100

    Paso a cloud

--- script_ejecucion_local_cloud.sh
+++ script_ejecucion_local_cloud.sh
@@ -41,35 +41,6 @@ if [ -z "$bucket_name" ]; then
     show_help
 fi
 
-# Configurar variables de entorno para Spark y Java
-echo "export JAVA_HOME=/usr/lib/jvm/default-java" >> /etc/profile.d/spark.sh
-echo "export PATH=\$PATH:/usr/local/spark/bin" >> /etc/profile.d/spark.sh
-
-# Actualizar paquetes y realizar instalación inicial
-apt-get update
-apt-get install -y default-jre python-is-python3 python3-venv
-
-# Descargar e instalar Apache Spark
-wget https://dlcdn.apache.org/spark/spark-3.5.3/spark-3.5.3-bin-hadoop3.tgz
-tar xzf spark-3.5.3-bin-hadoop3.tgz
-mv spark-3.5.3-bin-hadoop3 /usr/local/spark
-rm spark-3.5.3-bin-hadoop3.tgz
-
-# Crear un entorno virtual de Python
-mkdir -p /opt/virtualenvs
-python3 -m venv /opt/virtualenvs/spark_env
-
-# Activar el entorno virtual e instalar dependencias
-source /opt/virtualenvs/spark_env/bin/activate
-if [ -f requirements.txt ]; then
-    pip install --upgrade pip
-    pip install -r requirements.txt
-else
-    echo "requirements.txt no encontrado. Por favor, proporcione el archivo en el directorio actual."
-fi
-
-deactivate
-
 # Función para ejecutar Spark y guardar resultados
 execute_spark_jobs() {
     local bucket_name=$1
