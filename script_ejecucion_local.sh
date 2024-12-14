e9ac09a58a7a49924e698aecfd2cce117d4e9681
Alberto12x <alberm33@ucm.es>
2024-12-14 21:29:59 +0100

    Local acabado

--- script_ejecucion_local.sh
+++ script_ejecucion_local.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Definimos los colores
+GREEN='\e[32m'
+RESET='\e[0m'
+
 # Definimos el nombre del entorno virtual y el directorio de salida
 VENV_DIR=".venvGroup9Cloud"
 OUTPUT_DIR="outputs"
@@ -9,7 +13,7 @@ PYTHON_VERSION="3.12"
 
 # Función para mostrar el mensaje de ayuda
 show_help() {
-    echo "Uso: ./script_ejecucion_local.sh <opciones>"
+    echo -e "${GREEN}Uso: ./script_ejecucion_local.sh <opciones>${RESET}"
     echo ""
     echo "Opciones:"
     echo "  -h, --help                   Muestra este mensaje de ayuda."
@@ -26,11 +30,11 @@ show_help() {
 # Función para comprobar si los argumentos fueron proporcionados correctamente
 check_arguments() {
     if [[ $# -lt 4 ]]; then
-        echo "Error: Se requieren al menos 4 argumentos. El formato es:"
+        echo -e "${GREEN}Error: Se requieren al menos 4 argumentos. El formato es:${RESET}"
         echo "./script_ejecucion_local.sh <ancho de banda para el filtro> <Comparador 1 > , 0 <= > <top k> <dataset>"
         exit 1
     fi
-    echo "Argumentos recibidos correctamente."
+    echo -e "${GREEN}Argumentos recibidos correctamente.${RESET}"
     # Guardamos los argumentos en variables
     FILTER_BANDWIDTH=$1
     COMPARATOR=$2
@@ -45,10 +49,10 @@ check_python_version() {
     PYTHON_MINOR_VERSION=$(echo $PYTHON_INSTALLED | cut -d ' ' -f2 | cut -d '.' -f2)
 
     if [[ "$PYTHON_MAJOR_VERSION" -ge 3 && "$PYTHON_MINOR_VERSION" -ge 12 ]]; then
-        echo "Python 3.12 o mayor está instalado."
+        echo -e "${GREEN}Python 3.12 o mayor está instalado.${RESET}"
     else
-        echo "Error: Python 3.12 o mayor no está instalado."
-        echo "Por favor, instala la versión adecuada o actualiza Python."
+        echo -e "${GREEN}Error: Python 3.12 o mayor no está instalado.${RESET}"
+        echo -e "${GREEN}Por favor, instala la versión adecuada o actualiza Python.${RESET}"
         exit 1
     fi
 }
@@ -56,29 +60,29 @@ check_python_version() {
 # Función para crear un entorno virtual si no está activo
 create_virtual_env() {
     if [[ -d $VENV_DIR ]]; then
-        echo "El entorno virtual '$VENV_DIR' existe. Eliminando..."
+        echo -e "${GREEN}El entorno virtual '$VENV_DIR' existe. Eliminando...${RESET}"
         rm -rf $VENV_DIR
     fi
 
-    echo "Creando entorno virtual..."
+    echo -e "${GREEN}Creando entorno virtual...${RESET}"
     python3 -m venv $VENV_DIR
     source $VENV_DIR/bin/activate
-    echo "Entorno virtual activado."
+    echo -e "${GREEN}Entorno virtual activado.${RESET}"
 }
 
 # Función para instalar las dependencias desde 'requirements.txt'
 install_requirements() {
     if [[ -f "requeriments.txt" ]]; then
-        echo "Instalando dependencias desde 'requirements.txt'..."
+        echo -e "${GREEN}Instalando dependencias desde 'requirements.txt'...${RESET}"
         python3 -m pip install -r requeriments.txt
         if [[ $? -eq 0 ]]; then
-            echo "Dependencias instaladas con éxito."
+            echo -e "${GREEN}Dependencias instaladas con éxito.${RESET}"
         else
-            echo "Hubo un error al instalar las dependencias."
+            echo -e "${GREEN}Hubo un error al instalar las dependencias.${RESET}"
             exit 1
         fi
     else
-        echo "No se encontró 'requirements.txt'."
+        echo -e "${GREEN}No se encontró 'requirements.txt'.${RESET}"
         exit 1
     fi
 }
@@ -86,52 +90,52 @@ install_requirements() {
 # Función para comprobar y eliminar el directorio 'outputs' si existe
 check_and_remove_outputs() {
     if [[ -d $OUTPUT_DIR ]]; then
-        echo "El directorio '$OUTPUT_DIR' existe. Eliminando..."
+        echo -e "${GREEN}El directorio '$OUTPUT_DIR' existe. Eliminando...${RESET}"
         rm -rf $OUTPUT_DIR
     else
-        echo "El directorio '$OUTPUT_DIR' no existe. Nada que eliminar."
+        echo -e "${GREEN}El directorio '$OUTPUT_DIR' no existe. Nada que eliminar.${RESET}"
     fi
 }
 
 # Función para comprobar si el archivo GeoLite2-City.mmdb existe y, si no, descargarlo
 check_and_download_geo_lite() {
     if [[ ! -f "$GEO_LITE_FILE" ]]; then
-        echo "El archivo '$GEO_LITE_FILE' no existe. Descargando..."
+        echo -e "${GREEN}El archivo '$GEO_LITE_FILE' no existe. Descargando...${RESET}"
         curl -L -o "$GEO_LITE_FILE" "$GEO_LITE_URL"
         if [[ $? -eq 0 ]]; then
-            echo "Archivo '$GEO_LITE_FILE' descargado con éxito."
+            echo -e "${GREEN}Archivo '$GEO_LITE_FILE' descargado con éxito.${RESET}"
         else
-            echo "Hubo un error al descargar el archivo '$GEO_LITE_FILE'."
+            echo -e "${GREEN}Hubo un error al descargar el archivo '$GEO_LITE_FILE'.${RESET}"
             exit 1
         fi
     else
-        echo "El archivo '$GEO_LITE_FILE' ya existe."
+        echo -e "${GREEN}El archivo '$GEO_LITE_FILE' ya existe.${RESET}"
     fi
 }
 
 # Función para ejecutar los programas de Python dentro del entorno virtual
 run_python_programs() {
-    echo "Ejecutando programas de Python dentro del entorno virtual..."
+    echo -e "${GREEN}Ejecutando programas de Python dentro del entorno virtual...${RESET}"
 
-    echo "Ejecutando script: df_codes/ancho_banda.py"
+    echo -e "${GREEN}Ejecutando script: df_codes/ancho_banda.py${RESET}"
     python3 df_codes/ancho_banda.py $DATASET $OUTPUT_DIR/output_anchobanda
 
-    echo "Ejecutando script: df_codes/frecuencia_protocolos.py"
+    echo -e "${GREEN}Ejecutando script: df_codes/frecuencia_protocolos.py${RESET}"
     python3 df_codes/frecuencias_protocolos.py $DATASET $OUTPUT_DIR/output_frecuencia_protocolos
 
-    echo "Ejecutando script: df_codes/inverted_index_flags.py"
+    echo -e "${GREEN}Ejecutando script: df_codes/inverted_index_flags.py${RESET}"
     python3 df_codes/inverted_index_flags.py $DATASET $OUTPUT_DIR/output_inverted_index
 
-    echo "Ejecutando script: df_codes/media_ancho_banda_protocolo.py"
+    echo -e "${GREEN}Ejecutando script: df_codes/media_ancho_banda_protocolo.py${RESET}"
     python3 df_codes/media_ancho_banda_protocolo.py $DATASET $OUTPUT_DIR/output_media_anchobanda_protocolos
 
-    echo "Ejecutando script: df_codes/filtro_ancho_banda.py"
+    echo -e "${GREEN}Ejecutando script: df_codes/filtro_ancho_banda.py${RESET}"
     python3 df_codes/filtro_ancho_banda.py $OUTPUT_DIR/output_anchobanda $OUTPUT_DIR/outputs_filtro_anchobanda $FILTER_BANDWIDTH $COMPARATOR
 
-    echo "Ejecutando script: df_codes/top_ancho_banda.py"
+    echo -e "${GREEN}Ejecutando script: df_codes/top_ancho_banda.py${RESET}"
     python3 df_codes/top_ancho_banda.py $OUTPUT_DIR/output_anchobanda $OUTPUT_DIR/outputs_top_anchobanda $TOP_K
 
-    echo "Ejecutando script: df_codes/ips_ubicacion"
+    echo -e "${GREEN}Ejecutando script: df_codes/ips_ubicacion${RESET}"
     python3 df_codes/ips_ubicacion.py $DATASET $OUTPUT_DIR/output_ip_ubicacion ./GeoLite2-City.mmdb
 }
 
@@ -141,6 +145,7 @@ if [[ "$1" == "-h" || "$1" == "--help" ]]; then
     exit 0
 fi
 
+echo -e "${GREEN}Script de ejecucion en local.${RESET}"
 # Comprobar los argumentos pasados al script
 check_arguments "$@"
 
@@ -162,14 +167,9 @@ check_and_remove_outputs
 # Ejecutar los programas de Python
 run_python_programs
 
-# Si el entorno virtual fue creado en este script, lo desactivamos y lo borramos
-if [[ -z "$VIRTUAL_ENV" ]]; then
-    deactivate
-    echo "Desactivando el entorno virtual."
 
-    # Borrar el entorno virtual si fue creado en este script
-    rm -rf $VENV_DIR
-    echo "El entorno virtual ha sido eliminado."
-else
-    echo "El entorno virtual activo no será desactivado ni eliminado."
-fi
+echo -e "${GREEN}Desactivando el entorno virtual.${RESET}"
+
+# Borrar el entorno virtual 
+rm -rf $VENV_DIR
+echo -e "${GREEN}El entorno virtual ha sido eliminado.${RESET}"
